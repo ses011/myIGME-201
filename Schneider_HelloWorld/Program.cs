1c70f5723d84049b9473bebc472472378b98ffa1
Sarah Schneider <sses7344@gmail.com>
2023-08-31 12:16:06 -0400

    PE1- Hello World

---
+++ Schneider_HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schneider_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Sarah Schneider");
+
+            //initialize values for two int variables
+            int a = 26;
+            int b = 19;
+
+            //display addition value of int variables
+            Console.WriteLine("a + b = " + (a + b).ToString());
+
+            //counts from 0 to a exclusive, printing each value
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
+ 
