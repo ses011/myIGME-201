06df32b1aea1b7544110098e5553a69e553008e7
Sarah Schneider <sses7344@gmail.com>
2023-09-10 12:04:46 -0400

    PE4 - Exercise 1 final

---
+++ PE4-Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE4_Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two integers: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            while (true) {
+                if (num1 > 10 ^ num2 > 10)
+                {
+                    Console.WriteLine(num1 + " and " + num2);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter two integers- one has to be greater than 10, and one less than: ");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
