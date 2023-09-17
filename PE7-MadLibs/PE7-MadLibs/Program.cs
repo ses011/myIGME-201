340b1cc5712a25c6df8c0f8a5b617cf015d50b51
Sarah Schneider <sses7344@gmail.com>
2023-09-17 19:16:29 -0400

    PE7- MadLibs WIP

--- PE7-MadLibs/PE7-MadLibs/Program.cs
+++ PE7-MadLibs/PE7-MadLibs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,26 +10,29 @@ using System.Threading.Tasks;
 namespace PE7_MadLibs {
     internal class Program {
         static void Main(string[] args) {
-            string temps = @"c:\templates\MadLibsTemplate.txt";
-            string[] aTemplates;
+            string fName = @"c:\templates\MadLibsTemplate.txt";
+            ArrayList alTemps = new ArrayList();
             int len = 0;
 
-            using (StreamReader sr = File.OpenText(temps)) {
+            // Reads in each line of the file and saves each as a seperate string to the alTemps ArrayList, and increases counter of how many total templates there are
+            using (StreamReader sr = File.OpenText(fName)) {
                 string line;
-                string templates = "";
                 
                 while((line = sr.ReadLine()) != null) {
-                    templates += line;
+                    alTemps.Add(line);
                     len++;
                 }
-                
             }
+
+            // Prompts user for their name
             string name;
             Console.WriteLine("Enter your name.");
             name = (Console.ReadLine());
 
+            // Prompts user for which MadLibs template they want to use
+            // Makes sure input is an integer within the range of 1-len
             int libChoice;
-            Console.WriteLine("Pick a MadLibs template (enter an int between 1 and " + len + ").");
+            Console.WriteLine($"Pick a MadLibs template (enter an int between 1 and {len}).");
             while (true) {
                 try {
                     libChoice = int.Parse(Console.ReadLine());
@@ -43,12 +47,25 @@ namespace PE7_MadLibs {
                     Console.WriteLine("Input must be an integer, enter new choice.");
                 }
                 catch {
-                    Console.WriteLine("Input must be between 1 and " + len + ", enter new choice");
+                    Console.WriteLine($"Input must be between 1 and {len}, enter new choice");
                 }
             }
-            // string libTemp = aTemplates[libChoice];
-            // Console.WriteLine(libTemp);
-            Console.WriteLine(aTemplates);
+
+            string resultString = "";
+            string tempChoice = (string)alTemps[len - 1];
+            string[] aTemplate = tempChoice.Split(' ');
+
+            foreach (string word in aTemplate) {
+                if (word.Substring(0, 1).Equals("{")) {
+                    string prompt = word.Substring(1, word.Length - 2);
+                    Console.WriteLine(prompt);
+                }
+                else {
+                    Console.WriteLine(word);
+                    resultString += word;
+                }
+            }
+            Console.WriteLine(resultString);
         }
     }
 }
