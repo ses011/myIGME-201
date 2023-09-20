8e3538995b7327384c16fea1aa211d24bc16671d
Sarah Schneider <sses7344@gmail.com>
2023-09-20 14:42:09 -0400

    PE8- Quotes around words in String fin

---
+++ PE8- QuoteStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8__QuoteStrings {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter a String: ");
+            string input = Console.ReadLine();
+
+            string[] aInput = input.Split(' ');
+            string output = "";
+            foreach (string s in aInput) {
+                output += $"\"{s}\" ";
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
