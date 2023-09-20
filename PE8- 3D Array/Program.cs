a492d537c647be02a13597c31a0d923de1993841
Sarah Schneider <sses7344@gmail.com>
2023-09-20 13:51:05 -0400

    PE8- Exercise 5 Fin

---
+++ PE8- 3D Array/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PE8__3D_Array {
+    internal class Program {
+        static void Main(string[] args) {
+            double[][,] result = new double[21][,];
+
+            //1st dimention counter
+            int f = 0;
+
+            for (double x = -1; x <= 1; x += 0.1) {
+                double[,] tempArray = new double[2, 31];
+
+                //3rd dimention counter
+                int i = 0;
+                for (double y = 1; y < 4.1; y += 0.1) {
+                    
+                    double z = (3 * Math.Pow(y, 2)) + (2 * x) - 1;
+                    Console.WriteLine(y);
+                    tempArray[0, i] = y;
+                    tempArray[1, i] = z;
+                    i++;
+                }
+
+                result[f] = tempArray;
+                //Console.WriteLine($"i = {i}");
+
+
+                f++;
+            }
+
+            Console.WriteLine((-0.1) + 0.1);
+
+            double inc = 0.1;
+            double xval = -1;
+            foreach (double[,] x in result) {
+                Console.WriteLine(" ----- x = " + Math.Round(xval, 2) + " -------");
+                for (int i = 0; i < x.Length/2; i++) {
+                    Console.WriteLine($"when y = {x[0, i]}, z = {x[1, i]}");
+                }
+
+                xval += inc;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
