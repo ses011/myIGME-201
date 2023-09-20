a29bc0c8bc0c28156cc71e05dc5ef9f213f0bb88
Sarah Schneider <sses7344@gmail.com>
2023-09-20 14:10:10 -0400

    PE8- Reversing a String fin

---
+++ PE8- ReverseStr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8__ReverseStr {
+    internal class Program {
+        static void Main(string[] args) {
+            // Prompts user and collects input
+            Console.WriteLine("Enter a string to be reversed: ");
+            string input = Console.ReadLine();
+
+            //Empty string for output
+            string reverse = "";
+            foreach (char c in input) {
+                // Adds c to the beginning
+                reverse = c + reverse;
+            }
+            
+            Console.WriteLine(reverse);
+        }
+    }
+}
