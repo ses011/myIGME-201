1cab82058dede527c2e452c345f0e12d70cd6648
Sarah Schneider <sses7344@gmail.com>
2023-10-01 20:42:43 -0400

    test1-12

---
+++ Test1-12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_12 {
+    internal class Program {
+        static void Main(string[] args) {
+            string sName;
+            double dSalary = 30000;
+
+            Console.WriteLine("Enter your name");
+            sName = Console.ReadLine();
+
+            if(GiveRaise(sName, ref dSalary)) {
+                Console.WriteLine($"Congrats on the raise to {dSalary}");
+            }
+        }
+
+        static bool GiveRaise(string name, ref double salary) {
+            if(name.ToLower().Equals("sarah")) {
+                salary += 19999.99;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
