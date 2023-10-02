b16c1f4ddc184a1b93cbbba91ea3dc6614cce6b0
Sarah Schneider <sses7344@gmail.com>
2023-10-01 20:06:13 -0400

    Test1-3 Delegates

---
+++ Test1-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_3 {
+    internal class Program {
+        //Delegate declaration
+        delegate double RoundDelegateBase(double x, int y);
+
+        static void Main(string[] args) {
+            //variable declaration and initialization
+            double x = 4.235893;
+            int y = 2;
+            RoundDelegateBase roundDelegate;
+            roundDelegate = new RoundDelegateBase(Math.Round);
+
+            //Call delegate and display result
+            double result = roundDelegate(x, y);
+            Console.WriteLine(result);
+        }
+    }
+}
