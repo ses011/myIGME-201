f8ba481d96c0037d35ecac90776a733234083707
Sarah Schneider <sses7344@gmail.com>
2023-10-11 01:13:25 -0400

    Vehicle clean-up and using classes with Traffic

---
+++ Traffic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic {
+    internal class Program {
+        static void Main(string[] args) {
+            SUV passengerCar = new SUV();
+            FreightTrain freightTrain = new FreightTrain();
+
+            AddPassenger(passengerCar);
+            // AddPassenger(freightTrain);  <- Doesn't work as freight train doesn't implement IPassengerCarrier
+        }
+        
+        static void AddPassenger (IPassengerCarrier carrier) {
+            carrier.LoadPassenger();
+            Console.WriteLine(carrier.ToString());
+
+        }
+    }
+}
