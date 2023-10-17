e35aec8d5f35f3ef1fb163ea7fa8c7fffac5010b
Sarah Schneider <sses7344@gmail.com>
2023-10-17 16:38:33 -0400

    PE-12.3 Final

---
+++ PE-12.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_12._3 {
+    internal class Program {
+        static void Main(string[] args) {
+            MyDerivedClass derivedObject = new MyDerivedClass("phrase");
+
+            Console.WriteLine(derivedObject.GetString());
+        }
+
+        public class MyClass {
+            private String myString;
+
+            public MyClass(String str) {
+                myString = str;
+            }
+
+            public virtual String GetString() {
+                return myString;
+            }
+
+            public String MyString {
+                get { 
+                    return myString; 
+                }
+                
+            }
+        }
+
+        public class MyDerivedClass : MyClass {
+
+            public MyDerivedClass(String str) : base(str) { }
+
+            public override String GetString() {
+                return base.GetString() + " (output from the derived class)";
+            }
+
+        }
+    }
+}
