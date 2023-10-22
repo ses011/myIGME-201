6da9da8d3ee4a2c07fc654c80eeaf0f6534d6d9d
Sarah Schneider <sses7344@gmail.com>
2023-10-22 17:49:50 -0400

    PE-14

---
+++ PE-14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_14 {
+    internal class Program {
+        static void Main(string[] args) {       
+
+            IInterface myClass = new MyClass();
+            IInterface myOtherClass = new MyOtherClass();
+
+            myClass.myMethod();
+            myOtherClass.myMethod();
+        }
+
+
+        public interface IInterface {
+            void myMethod();
+        }
+        public class MyClass : IInterface {
+            private string name = "Sarah";
+
+            public void myMethod() {  
+                Console.WriteLine(name); 
+            }
+        }
+
+        public class MyOtherClass : IInterface {
+            private string name = "Heather";
+
+            public void myMethod() { 
+                Console.WriteLine(name);  
+            }
+        }
+    }
+}
