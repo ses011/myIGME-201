4bc69a2392bce22a4274a0e99eb7c59e835dc787
Sarah Schneider <ses1431@rit.edu>
2023-11-28 23:05:06 -0500

    PE-17 Guessing Game

---
+++ PE17_NumberGuesser/PE17_NumberGuesser/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PE17_NumberGuesser
+{
+    public partial class Range : Form
+    {
+        public Range()
+        {
+            InitializeComponent();
+
+            startButton.Click += new EventHandler(StartButton__Click);
+            lowTextBox.KeyPress += new KeyPressEventHandler(LowTextBox__KeyPress);
+            highTextBox.KeyPress += new KeyPressEventHandler(HighTextBox__KeyPress);
+            
+        }
+
+        private void StartButton__Click(object sender, EventArgs e)
+        {
+            int lowNum;
+            int highNum;
+
+            // Try to save low and high nums as ints
+            // if it works and low is < high, make new game form and return
+            try
+            {
+                lowNum = Int32.Parse(lowTextBox.Text);
+                highNum = Int32.Parse(highTextBox.Text);
+
+
+                if (lowNum < highNum)
+                {
+                    GameForm gameForm = new GameForm(lowNum, highNum);
+                    gameForm.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Low number is larger than high number.");
+                }
+                return;
+            }
+            // If it doesn't work display error message
+            catch
+            {
+                MessageBox.Show("The numbers are invalid.");
+            }
+
+        }
+
+        private void LowTextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void HighTextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ PE17_NumberGuesser/PE17_NumberGuesser/GameForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PE17_NumberGuesser
+{
+    public partial class GameForm : Form
+    {
+        int num;
+        int guesses = 0;
+
+        public GameForm(int lowNum, int highNum)
+        {
+            InitializeComponent();
+
+            // Generate number to guess
+            Random rand = new Random();
+            num = rand.Next(lowNum, highNum + 1);
+            this.timer1.Enabled = true;
+
+            this.guessButton.Click += new EventHandler(GuessButton__Click);
+            //this.guessButton.KeyPress += new KeyPressEventHandler(GuessButton__KeyPress);
+            this.timer1.Tick += new EventHandler(Timer__Tick);
+        }
+
+
+        private void TimesUp()
+        {
+            // Timer has exceeded 45 seconds
+            this.guessTextBox.Enabled = false;
+            this.guessButton.Enabled = false;
+
+            MessageBox.Show($"Times up! The number was {num}");
+            this.Close();
+        }
+
+
+        private void GuessButton__Click(object sender, EventArgs e)
+        {
+            int guess = Int32.Parse(guessTextBox.Text);
+            if (guess == num)
+            {
+                // Display success messages
+                guesses++;
+                this.outputLabel.ForeColor = Color.Green;
+                this.outputLabel.Text = "Correct!";
+                this.timer1.Enabled = false;
+                MessageBox.Show($"You got it in {guesses} attempts!");
+
+                this.Close();
+            }
+            // Changes hint if guess is incorrect
+            else if(guess < num)
+            {
+                this.outputLabel.Text = $"{guess} is too low";
+                this.guesses++;
+            }
+            else
+            {
+                this.outputLabel.Text = $"{guess} is too high";
+                guesses++;
+            }
+        }
+
+        private void Timer__Tick(object sender, EventArgs e)
+        {
+            this.TimerToolStripProgressBar.PerformStep();
+
+            // Progress bar increases by 5 every half second, making 450 the value for 45 seconds
+            if (this.TimerToolStripProgressBar.Value >= 450)
+            {
+                this.timer1.Enabled = false;
+                TimesUp();
+            }
+        }
+    }
+}
