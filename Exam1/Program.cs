20474c60cc0df04caa7c46b975c2b90c211ee107
Sarah Schneider <sses7344@gmail.com>
2023-12-18 15:15:01 -0500

    Final questions 1-2

---
+++ Exam1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * IGME-201 Final Q1-2
+ * 
+ * MyStack -  building own stack structure
+ * 
+ * MyQueue - building own queue structure
+ * 
+ */
+namespace Exam1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /*
+     * Rewriting a stack with push, pop, and peek methods 
+     */
+    class MyStack
+    {
+        List<int> list;
+
+        public MyStack()
+        {
+            this.list = new List<int>();
+        }
+
+        public void Push(int n)
+        {
+            this.list.Add(n);
+        }
+
+        public int Pop()
+        {
+            int val = this.list.Last();
+            this.list.RemoveAt(this.list.Count - 1);
+            return val;
+        }
+
+        public int Peek() 
+        { 
+            return this.list[this.list.Count - 1]; 
+        }
+    }
+
+    /*
+     * Rewriting a queue with enqueue, dequeue, and peek methods 
+     */
+    class MyQueue
+    {
+        List<int> list;
+        public MyQueue()
+        {
+            this.list= new List<int>();
+        }
+
+        public void Enqueue(int n)
+        {
+            this.list.Add(n);
+        }
+
+        public int Dequeue()
+        {
+            int val = this.list[0];
+            this.list.RemoveAt(0);
+            return val;
+        }
+
+        public int Peek()
+        {
+            return this.list[0];
+        }
+    }
+}
