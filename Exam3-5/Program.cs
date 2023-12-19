4cf982cf5c2b6cdca09bdc4fbf733ca6184fe182
Sarah Schneider <sses7344@gmail.com>
2023-12-18 23:44:55 -0500

    Rest of Q3 and Q5 (Dijkstra's)

--- Exam3-5/Program.cs
+++ Exam3-5/Program.cs
@@ -12,11 +12,14 @@ namespace Exam3_5
         {
             List<Node> nodes = SetNodes();
 
-
-
+            List<Node> dijkstra = Dijkstra(nodes, nodes[0]);
             
+            foreach (Node node in dijkstra)
+            {
+                Console.WriteLine(node.name);
+            }
         }
-        
+
         // Hard code the Node/Edge values and directions
         // Display adjacency list and matrix
         public static List<Node> SetNodes()
@@ -103,10 +106,10 @@ namespace Exam3_5
 
             string label = "R  B  G  L  Y  P  G  O";
             Console.Write("\n " + label);
-            for (int i = 0; i < nodes.Count; i ++)
+            for (int i = 0; i < nodes.Count; i++)
             {
                 Console.Write($"\n{label.Substring((i * 3), 1)}");
-                for (int j = 0; j < nodes.Count; j ++)
+                for (int j = 0; j < nodes.Count; j++)
                 {
                     Console.Write($" {matrix[i, j]}");
                 }
@@ -120,41 +123,81 @@ namespace Exam3_5
 
         /*
          * Depth first search algorithm
-         *//*
-        public List<string> DFS() 
-        {
-            
-        }
 
-        *//*
-         * Dijkstra's shortest path algorithm
-         *//*
-        public List<string> Dijkstra()
+
+        public List<string> DFS()
         {
+
+        }
 */
-    }
 
-    // Node class, has color string and list of Edges leaving it
-    public class Node
-    {
-        public string name;
-        public List<Edge> edges = new List<Edge>();
-        public Node(string name)
+        /* Dijkstra's shortest path algorithm - recursively finds cheapest path
+        */
+        public static List<Node> Dijkstra(List<Node> nodes, Node root)
         {
-            this.name = name;
+            List<Node> list = new List<Node>();
+            Console.WriteLine("\n\nDijkstra's");
+            while (nodes.Count > 0)
+            {
+                // Checks that nodes list contains edge end point (neighbor)
+                foreach (Edge edge in root.edges)
+                {
+                    if (nodes.Contains(edge.end) && edge.val < edge.end.distance)
+                    {
+                        if (edge.end.distance == int.MaxValue)
+                        {
+                            edge.end.distance = edge.val;
+                        }
+                        else
+                        {
+                            edge.end.distance += edge.val;
+                        }
+                    }
+                }
+                nodes.Remove(root);
+                root.finished = true;
+                list.Add(root);
+
+                root = null;
+                foreach (Node node in nodes)
+                {
+                    if (root == null || node.distance < root.distance)
+                    {
+                        root = node;
+                    }
+                }
+            }
+            return list;
         }
-    }
 
-    // Edge class, has edge length (for dijkstra's) and the node it's pointing to
-    public class Edge
-    {
-        public int val;
-        public Node end;
-
-        public Edge(int val, Node end)
+        // Node class, has color string and list of Edges leaving it
+        public class Node
         {
-            this.val = val;
-            this.end = end;
+            public string name;
+            public List<Edge> edges = new List<Edge>();
+
+            //For Dijkstra's
+            public Boolean finished = false;
+            public int distance = int.MaxValue;
+
+            public Node(string name)
+            {
+                this.name = name;
+            }
+        }
+
+        // Edge class, has edge length (for dijkstra's) and the node it's pointing to
+        public class Edge
+        {
+            public int val;
+            public Node end;
+
+
+            public Edge(int val, Node end)
+            {
+                this.val = val;
+                this.end = end;
+            }
         }
     }
 }
